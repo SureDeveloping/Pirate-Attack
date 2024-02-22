36423f9304f4ec00c5bcc172f95dad712f815a0a
Stephan Sure <stephan.sure@gmail.com>
2024-02-22 17:59:34 +0000

    Add story text to the start screen

--- run.py
+++ run.py
@@ -13,3 +13,10 @@ user_name = input("Please enter your Name Commander:")
 while not user_name.strip() or not user_name.isalpha():
     print("The text field must not be left blank and only letters are permitted!")
     user_name = input("Please enter your Name Commander:")
+print()
+print(f"Commander {user_name} I have a mission for you from the King!\n"
+"Recently, our coastal cities and trade routes have been threatened by a new, dangerous pirate fleet.\n" 
+"Your task is to find the fleet and eliminate the threat.\n")
+menu_selection = input(f"Comander {user_name}, do you accept the challenge to sink the pirates and play then press P.\n"
+"For a detailed briefing on the strength of the pirate fleet and to read the rules, press R. \n"
+"If your are to terrified of the pirates and want to quit press Q.")
