8fbe510fab51628d69bc1a922a046689737b6f05
Stephan Sure <stephan.sure@gmail.com>
2024-02-22 17:52:54 +0000

    Add start screen and name input with valid data check.

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+import time
+from art import *
+tprint("Pirate Attack",font="cola",chr_ignore=True)
+
+print('Welcome to Pirate Attack, the Game!\n')
+
+user_name = input("Please enter your Name Commander:")
+# The strip() method ensures that something has to be entered and the isalpha() method ensures that no numbers are entered 
+while not user_name.strip() or not user_name.isalpha():
+    print("The text field must not be left blank and only letters are permitted!")
+    user_name = input("Please enter your Name Commander:")
